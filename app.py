31a670f52719f499a8d46267ce52020b5afc2fd5
sanjiv kannaa jeganathan <jsanjiv2003@gmail.com>
2022-08-17 16:52:35 +0530

    finall

--- app.py
+++
@@ -1,5 +0,0 @@
-from werkzeug.middleware.dispatcher import DispatcherMiddleware # use to combine each Flask app into a larger one that is dispatched based on prefix
-from server1 import app as server1
-from server2 import app as server2
-from server3 import app as server3
-application = DispatcherMiddleware(server1, {'/server2': server2}, {'/server3': server3})
